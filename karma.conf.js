2b4408cd96b7dbb40bf93677697348d44857b423
João Paulo Frezzarin <30581142+jpfrezzarin@users.noreply.github.com>
2021-02-08 21:13:04 -0300

    Removed customLaunchers

--- karma.conf.js
+++ karma.conf.js
@@ -26,12 +26,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome','ChromeHeadless'],
-    singleRun: false,
-    customLaunchers: {
-      ChromeHeadless: {
-        base: 'ChromeHeadless',
-        flags: [ '--no-sandbox' ]
-      }
-    }
+    singleRun: false
   });
 };
